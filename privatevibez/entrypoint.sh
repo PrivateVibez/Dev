dd2154492361aa744b556f1df3b30c61c04d4d1e
Asakil <asakilsarhan@gmail.com>
2023-10-19 20:26:02 +0800

    bug fix

--- privatevibez/entrypoint.sh
+++ privatevibez/entrypoint.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
-# Path to a sentinel file which indicates that the initial setup was done
+#!/bin/sh
 SENTINEL_FILE="/privatevibez/.initial_setup_done"
-# Check if the sentinel file exists
 if [ ! -f "$SENTINEL_FILE" ]; then
-    # Run initialization commands
     python manage.py migrate || exit 1
     python manage.py cities --import=country,region,city || exit 1
     python manage.py loaddata accounts/fixtures/staffdata.json || exit 1
-
-    # Create the sentinel file to mark the initial setup as done
     touch $SENTINEL_FILE
 fi
-
 exec "$@"
