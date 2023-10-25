5eadb2587adefed0963de4e02ef35b7cffbf06fc
Asakil <asakilsarhan@gmail.com>
2023-10-25 08:36:29 +0800

    fix

--- privatevibez/entrypoint.sh
+++ privatevibez/entrypoint.sh
@@ -2,7 +2,6 @@
 SENTINEL_FILE="/privatevibez/.initial_setup_done"
 if [ ! -f "$SENTINEL_FILE" ]; then
     python manage.py migrate || exit 1
-    python manage.py cities --import=country,region,city || exit 1
     python manage.py loaddata accounts/fixtures/staffdata.json || exit 1
     touch $SENTINEL_FILE
 fi
